8aef3bc8aca0d522172ed3615922b648e18a69b7
Kitsune1890 <zyildiz966@gmail.com>
2022-12-17 17:52:22 +0300

    changed sys

--- Petiary/src/mainAndSys/PetiarySys.java
+++ Petiary/src/mainAndSys/PetiarySys.java
@@ -1,5 +1,9 @@
 package mainAndSys;
 
+import java.util.HashSet;
+
+import isA.Pet;
+
 public class PetiarySys {
-	private static HashSet<> 
+	private static HashSet<Pet> pets = new HashSet<Pet>();
 }
