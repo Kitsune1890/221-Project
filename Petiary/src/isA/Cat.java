935814657432efa06ddf597a9fe5c65427a7ecc7
Kitsune1890 <zyildiz966@gmail.com>
2022-12-19 12:23:55 +0300

    deleted car size

--- Petiary/src/isA/Cat.java
+++ Petiary/src/isA/Cat.java
@@ -4,14 +4,9 @@ import java.util.Calendar;
 import hasA.*;
 
 public class Cat extends Pet{
-	private String catSize;
 
 	public Cat(int id, String name, String breed, Calendar bDate, String illness, Vet vet, double weight) {
 		super(id, name, breed, bDate, illness, vet, weight);
-	}
-
-	public String getCatSize() {
-		return catSize;
 	}
 
 	@Override
@@ -22,8 +17,7 @@ public class Cat extends Pet{
 				+ "\nillness=" + illness
 				+ "\nbirth date:" + bDate.get(Calendar.DAY_OF_MONTH)+"."+ bDate.get(Calendar.MONTH)+"."+bDate.get(Calendar.YEAR)
 				+ "\nNext checkup=" + checkup.get(Calendar.DAY_OF_MONTH)+"."+ checkup.get(Calendar.MONTH)+"."+checkup.get(Calendar.YEAR) 
-				+ "\nweight=" + weight
-				+"\ncatSize=" + catSize; 
+				+ "\nweight=" + weight;
 	}
 	
 	
