f4624515ec14ab31dedf80cd676deca826d821f9
Eren Yeşiltepe <eren.bilkentk15@hotmail.com>
2022-12-19 18:50:14 +0300

    h

--- Petiary/src/mainAndSys/PetiaryMain.java
+++ Petiary/src/mainAndSys/PetiaryMain.java
@@ -8,25 +8,7 @@ public class PetiaryMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-//for test purpose
-		
-		boolean[] arr = {true, true, true, true, true, true, true};
-		
-		PetiarySys.addVet("haha", "ankara", "23423424", arr);
-		
-		PetiarySys.getVacTemplate().add(new Vaccination(111,  "fvdv",5,  2));
-		PetiarySys.addCat(111,"tekir", "mahmut", Calendar.getInstance(), "", PetiarySys.searchVet("haha"), 10);
-		PetiarySys.addVaccination(PetiarySys.searchPet("111"),111,Calendar.getInstance());
-		System.out.println("dvsklvkl"+PetiarySys.displayVac(PetiarySys.searchPet("111")));
 
-		PetiarySys.searchPet("111").calcNextCheckup();
-		System.out.println(PetiarySys.displayPet());
-		
-		
-		//PetiarySys.addVet("ahmet", "çanakkale", "555", new boolean[]{true,true,true,true,true,true,true});
-		//
 	}
 
 }
