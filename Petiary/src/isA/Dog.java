a2bca7d7696b9b011827db96dd2752c1b6a3776b
Kitsune1890 <zyildiz966@gmail.com>
2022-12-29 18:28:57 +0300

    fixed wrong date problem

--- Petiary/src/isA/Dog.java
+++ Petiary/src/isA/Dog.java
@@ -18,8 +18,8 @@ public class Dog extends Pet {
 					+ "\nName: " + name 
 					+ "\nBreed: " + breed 
 					+ "\nIllness: " + illness
-					+ "\nBirth date: " + bDate.get(Calendar.DAY_OF_MONTH)+"."+ bDate.get(Calendar.MONTH)+"."+bDate.get(Calendar.YEAR)
-					+ "\nNext checkup -> " + checkup.get(Calendar.DAY_OF_MONTH)+"."+ checkup.get(Calendar.MONTH)+"."+checkup.get(Calendar.YEAR)
+					+ "\nBirth date: " + sdf.format(bDate.getTime())
+					+ "\nNext checkup -> " + sdf.format(checkup.getTime())
 					+ "\nVeterinary: " + vet.getName()
 					+ "\nWeight: " + weight
 					+ "\nDog Size: " + dogSize; 
