735475bbe229fa3210017d9370c88140f32f91dc
Egekorkmz <ege.korkmaz@ug.bilkent.edu.tr>
2022-12-29 14:57:43 +0300

    ğ

--- Petiary/src/interfaces/WeightCheck.java
+++ Petiary/src/interfaces/WeightCheck.java
@@ -1,6 +1,9 @@
 package interfaces;
 
+import java.text.SimpleDateFormat;
+
 public interface WeightCheck {
+	final static SimpleDateFormat sdf = new SimpleDateFormat("dd/M/yyyy");
 	
 	public boolean checkWeight();
 	
